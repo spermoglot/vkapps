3ccbafe460f6b4abd5d7d0f1e128d5ebb3d7796d
spermoglot <pahtusovde@gmail.com>
2019-04-03 22:00:55 +0300

    Updates

---
+++ precache-manifest.d7dd07f24776d5873410453b637ba344.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88e0734f1967e51210a6",
+    "url": "/vkapps/static/js/runtime~main.20472283.js"
+  },
+  {
+    "revision": "336742cb91827c5a6c3b",
+    "url": "/vkapps/static/js/main.d875617e.chunk.js"
+  },
+  {
+    "revision": "9d8a4c1f3d6dc9596c54",
+    "url": "/vkapps/static/js/2.f4357f78.chunk.js"
+  },
+  {
+    "revision": "9d8a4c1f3d6dc9596c54",
+    "url": "/vkapps/static/css/2.385e07b4.chunk.css"
+  },
+  {
+    "revision": "eab5693eb007b246262fb1b79b1f92e0",
+    "url": "/vkapps/index.html"
+  }
+];
