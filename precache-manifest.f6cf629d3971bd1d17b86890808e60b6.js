4e7d275ec5d54f07a79a4d00712efba0e3e945d9
spermoglot <pahtusovde@gmail.com>
2019-04-03 21:58:50 +0300

    Updates

---
+++ precache-manifest.f6cf629d3971bd1d17b86890808e60b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "641b487d12e13f00e756",
+    "url": "/vk-apps/static/js/runtime~main.47629f05.js"
+  },
+  {
+    "revision": "336742cb91827c5a6c3b",
+    "url": "/vk-apps/static/js/main.d875617e.chunk.js"
+  },
+  {
+    "revision": "9d8a4c1f3d6dc9596c54",
+    "url": "/vk-apps/static/js/2.f4357f78.chunk.js"
+  },
+  {
+    "revision": "9d8a4c1f3d6dc9596c54",
+    "url": "/vk-apps/static/css/2.385e07b4.chunk.css"
+  },
+  {
+    "revision": "53ad9c0068d5c0e18942b59c51c948cd",
+    "url": "/vk-apps/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vkapps/precache-manifest.d7dd07f24776d5873410453b637ba344.js"
+  "/vk-apps/precache-manifest.f6cf629d3971bd1d17b86890808e60b6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vkapps/index.html", {
+workbox.routing.registerNavigationRoute("/vk-apps/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
