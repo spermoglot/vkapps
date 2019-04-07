c54a11c60b51284fdd1c79af14722cecf20a0179
spermoglot <pahtusovde@gmail.com>
2019-04-08 02:01:59 +0300

    Updates

---
+++ precache-manifest.9a3db3a01be63bba7345d90613346647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88e0734f1967e51210a6",
+    "url": "/vkapps/static/js/runtime~main.20472283.js"
+  },
+  {
+    "revision": "0aa9be6dce80ddcbe45c",
+    "url": "/vkapps/static/js/main.6f798b5e.chunk.js"
+  },
+  {
+    "revision": "d16bf1c684e2e76a824a",
+    "url": "/vkapps/static/js/2.5adddce3.chunk.js"
+  },
+  {
+    "revision": "d16bf1c684e2e76a824a",
+    "url": "/vkapps/static/css/2.385e07b4.chunk.css"
+  },
+  {
+    "revision": "5cf06ec75523808445b626bd12bb5db2",
+    "url": "/vkapps/index.html"
+  }
+];
